3503ce5ffacfa743f2663816fa599889a71c8a79
mlauderb <mlauderb@iastate.edu>
2018-10-20 13:20:32 -0500

    working on issues

--- src/js/grid.js
+++ src/js/grid.js
@@ -1,31 +1,31 @@
-var origin = null;
 var mousePt = null;
-var gridSize = 20;
+var origin = null;
 
 function Grid() {
 
 	origin = new Point(0,0);
 	mousePt = new Point(0,0);
+	this.gridSize = 20;
 
-	this.show = function() {
+	this.draw = function() {
 		stroke(175);
 		let offsetx = origin.x;
 		let offsety = origin.y;
-		while (offsetx < canvasWidth) {
-			line(offsetx, 0, offsetx, canvasHeight);
-			offsetx += gridSize;
+		while (offsetx < canvas.width) {
+			line(offsetx, 0, offsetx, canvas.height);
+			offsetx += this.gridSize;
 		}
 		while (offsetx > 0) {
-			line(offsetx, 0, offsetx, canvasHeight);
-			offsetx -= gridSize;
+			line(offsetx, 0, offsetx, canvas.height);
+			offsetx -= this.gridSize;
 		}
-		while (offsety < canvasHeight) {
-			line(0, offsety, canvasWidth, offsety);
-			offsety += gridSize;
+		while (offsety < canvas.height) {
+			line(0, offsety, canvas.width, offsety);
+			offsety += this.gridSize;
 		}
 		while (offsety > 0) {
-			line(0, offsety, canvasWidth, offsety);
-			offsety -= gridSize;
+			line(0, offsety, canvas.width, offsety);
+			offsety -= this.gridSize;
 		}
 	}
 }
@@ -35,8 +35,10 @@ function mousePressed() {
 }
 
 function mouseDragged() {
-	let dx = mouseX - mousePt.x;
-	let dy = mouseY - mousePt.y;
-	origin.setPoint(origin.x + dx, origin.y + dy);
-	mousePt.setPoint(mouseX, mouseY);
+	if (mouseButton == CENTER) {
+		let dx = mouseX - mousePt.x;
+		let dy = mouseY - mousePt.y;
+		origin.setPoint(origin.x + dx, origin.y + dy);
+		mousePt.setPoint(mouseX, mouseY);
+	}
 }
--- src/js/workspace.js
+++ src/js/workspace.js
@@ -1,21 +1,44 @@
-var canvasWidth = 0;
-var canvasHeight = 0;
+var canvasdiv = null;
 var grid = null;
+var canvas = null;
 
 function setup() {
-	const canvasdiv = select("#workspacediv");
-	canvasWidth = canvasdiv.width;
-	canvasHeight = canvasdiv.height;
+	canvasdiv = select("#workspacediv");
+	let canvasWidth = canvasdiv.width;
+	let canvasHeight = canvasdiv.height;
 
-	var canvas = createCanvas(canvasWidth, canvasHeight);
+	canvas = createCanvas(canvasWidth, canvasHeight);
 	canvas.parent("workspacediv");
 	grid = new Grid();
 }
 
 function draw() {
-
 	background(255);
 
-	grid.show();
+	// canvas.height = document.getElementById("workspacediv").clientHeight;
+	// canvas.width = document.getElementById("workspacediv").clientWidth;
 
+	// console.log(document.getElementById("workspacediv").clientHeight);
+
+	grid.draw();
+
+}
+
+function Point(x, y) {
+	this.x = x;
+	this.y = y;
+
+	this.setX = function(x) {
+		this.x = x;
+	}
+
+	this.setY = function(y) { 
+		this.y = y;
+	}
+
+	this.setPoint = function(x,y) {
+		this.setX(x);
+		this.setY(y);
+	}
+	
 }
