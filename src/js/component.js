4d6271bf018c5be8a7f96cb6edfe2ba2a5dc5795
mlauderb <mlauderb@iastate.edu>
2018-10-21 00:39:50 -0500

    can place and gate with mouse.  still need to adjust for pan

---
+++ src/js/component.js
@@ -0,0 +1,123 @@
+inherits = function(ctor, superCtor) {
+  ctor.super_ = superCtor;
+  ctor.prototype = Object.create(superCtor.prototype, {
+    constructor: {
+      value: ctor,
+      enumerable: false,
+      writable: true,
+      configurable: true
+    }
+  });
+};
+// base component (always has a position)
+var Component = function(x, y) {
+	this.x = x;
+	this.y = y;
+}
+Component.prototype.setXY = function(x, y) {
+	this.x = x;
+	this.y = y;
+}
+
+/*
+ * gates here
+ */
+// base gate (always has 2 inputs)
+var Gate = function(x, y) {
+	Gate.super_.call(this, x, y);
+	this.gateList = [];
+	this.nodeA = false;
+    this.nodeB = false;
+    this.out = false;
+}
+inherits(Gate, Component);
+Gate.prototype.setInputNodes = function(nodeA, nodeB) {
+	this.nodeA = nodeA;
+    this.nodeB = nodeB;
+}
+
+// And Gate
+var AndGate = function(x, y) {
+	AndGate.super_.call(this, x, y);
+}
+inherits(AndGate, Gate);
+AndGate.prototype.getOutput = function() {
+	return this.nodeA && this.nodeB;
+}
+AndGate.prototype.getDrawingDimens = function() {
+	let dimens = [
+		[grid.getOrigin().x + this.x, grid.getOrigin().y + this.y, grid.getOrigin().x + this.x, grid.getOrigin().y + (6 * grid.getGridSize()) + this.y], // first line
+		[grid.getOrigin().x + this.x, grid.getOrigin().y + this.y, grid.getOrigin().x + 2*grid.getGridSize() + this.x, grid.getOrigin().y + this.y], // second line
+		[grid.getOrigin().x + this.x, grid.getOrigin().y + (6 * grid.getGridSize()) + this.y, grid.getOrigin().x + 2*grid.getGridSize() + this.x, grid.getOrigin().y + (6 * grid.getGridSize()) + this.y], // third line
+		[grid.getOrigin().x + 2*grid.getGridSize() + this.x, grid.getOrigin().y + grid.getGridSize() * 3 + this.y, 6 * grid.getGridSize(), 6 * grid.getGridSize(), -HALF_PI, HALF_PI], // arc
+		[grid.getOrigin().x + this.x, grid.getOrigin().y + grid.getGridSize() + this.y, grid.getOrigin().x - 2*grid.getGridSize() + this.x, grid.getOrigin().y + grid.getGridSize() + this.y], // nodeA
+		[grid.getOrigin().x + this.x, grid.getOrigin().y +5*grid.getGridSize() + this.y, grid.getOrigin().x - 2*grid.getGridSize() + this.x, grid.getOrigin().y + 5*grid.getGridSize() + this.y], // nodeB
+		[grid.getOrigin().x + 5*grid.getGridSize() + this.x, grid.getOrigin().y + 3*grid.getGridSize() + this.y, grid.getOrigin().x + 7*grid.getGridSize() + this.x, grid.getOrigin().y + 3*grid.getGridSize() + this.y] // out
+	];
+
+	return dimens;
+}
+
+// Or Gate
+var OrGate = function(x, y) {
+	OrGate.super_.call(this, x, y);
+}
+inherits(OrGate, Gate);
+OrGate.prototype.getOutput = function() {
+	return this.nodeA || this.nodeB;
+}
+// OrGate.prototype.getDrawingDimens = function() {
+	
+// }
+
+// Xor Gate
+var XorGate = function(x, y) {
+	XorGate.super_.call(this, x, y);
+}
+inherits(XorGate, Gate);
+XorGate.prototype.getOutput = function() {
+	return this.nodeA ? !this.nodeB : this.nodeB;
+}
+// XorGate.prototype.getDrawingDimens = function() {
+	
+// }
+
+// Nand Gate
+var NandGate = function(x, y) {
+	NandGate.super_.call(this, x, y);
+}
+inherits(NandGate, Gate);
+NandGate.prototype.getOutput = function() {
+	return !(this.nodeA && this.nodeB);
+}
+// NandGate.prototype.getDrawingDimens = function() {
+	
+// }
+
+// Nor Gate
+var NorGate = function(x, y) {
+	NorGate.super_.call(this, x, y);
+}
+inherits(NorGate, Gate);
+NorGate.prototype.getOutput = function() {
+	return !(this.nodeA || this.nodeB);
+}
+// NorGate.prototype.getDrawingDimens = function() {
+	
+// }
+
+// Xnor Gate
+var XnorGate = function(x, y) {
+	XnorGate.super_.call(this, x, y);
+}
+inherits(XnorGate, Gate);
+XnorGate.prototype.getOutput = function() {
+	return !(this.nodeA ? !this.nodeB : this.nodeB);
+}
+// XnorGate.prototype.getDrawingDimens = function() {
+	
+// }
+
+/*
+ * other components here
+ */
--- src/js/workspace.js
+++ src/js/workspace.js
@@ -2,6 +2,8 @@ var canvasdiv = null;
 var grid = null;
 var canvas = null;
 
+var gateList = [];
+
 function setup() {
 	canvasdiv = select("#workspacediv");
 	let canvasWidth = canvasdiv.width;
@@ -15,8 +17,32 @@ function setup() {
 function draw() {
 	background(255);
 
-	grid.draw();
+	grid.drawGrid();
 
+	// draw sample and gate for use later.
+	let x = 500;
+	let y = 300;
+	stroke(0);
+	strokeWeight(1.25);
+	var i;
+	// draw all gates
+	noFill();
+	for (i = 0; i < gateList.length; i++) {
+		line(gateList[i].getDrawingDimens()[0][0], gateList[i].getDrawingDimens()[0][1], gateList[i].getDrawingDimens()[0][2], gateList[i].getDrawingDimens()[0][3]);
+		line(gateList[i].getDrawingDimens()[1][0], gateList[i].getDrawingDimens()[1][1], gateList[i].getDrawingDimens()[1][2], gateList[i].getDrawingDimens()[1][3]);
+		line(gateList[i].getDrawingDimens()[2][0], gateList[i].getDrawingDimens()[2][1], gateList[i].getDrawingDimens()[2][2], gateList[i].getDrawingDimens()[2][3]);
+		arc(gateList[i].getDrawingDimens()[3][0], gateList[i].getDrawingDimens()[3][1], gateList[i].getDrawingDimens()[3][2], gateList[i].getDrawingDimens()[3][3], gateList[i].getDrawingDimens()[3][4], gateList[i].getDrawingDimens()[3][5]);
+
+		line(gateList[i].getDrawingDimens()[4][0], gateList[i].getDrawingDimens()[4][1], gateList[i].getDrawingDimens()[4][2], gateList[i].getDrawingDimens()[4][3]);
+		line(gateList[i].getDrawingDimens()[5][0], gateList[i].getDrawingDimens()[5][1], gateList[i].getDrawingDimens()[5][2], gateList[i].getDrawingDimens()[5][3]);
+		line(gateList[i].getDrawingDimens()[6][0], gateList[i].getDrawingDimens()[6][1], gateList[i].getDrawingDimens()[6][2], gateList[i].getDrawingDimens()[6][3]);
+	}
+}
+
+function mouseClicked() {
+	if (mouseButton == LEFT) {
+		gateList.push(new AndGate(mouseX, mouseY));
+	}
 }
 
 function windowResized() {
@@ -41,3 +67,4 @@ function Point(x, y) {
 	}
 	
 }
+
